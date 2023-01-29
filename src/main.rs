2da03322f7938da56b14d9a2d853756d5026e5b0
Guy Marshall <guymarshall9@gmail.com>
2023-01-29 11:01:08 +0000

    Add main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+
+fn to_sentence_case(word: &str) -> String {
+    let mut chars = word.chars();
+    match chars.next() {
+        None => String::new(),
+        Some(first_char) => first_char.to_uppercase().chain(chars).collect(),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_file = File::open("input.txt").unwrap();
+    let names = BufReader::new(input_file)
+        .lines()
+        .map(Result::unwrap)
+        .map(to_sentence_case)
+        .collect::<Vec<String>>();
+
+    let mut output_file = File::create("output.txt").unwrap();
+    for name in names {
+        writeln!(output_file, "{}", name).unwrap();
+    }
 }
